356f269e72121b00e2bd0c0c86c3c7d2466dc82f
skoenen <stefan.koenen@uni-duesseldorf.de>
2013-06-19 11:13:26 +0200

    Preparation for the storage handling for the measurements on server side.

---
+++ pylofer/storage/__init__.py
@@ -0,0 +1,6 @@
+from storage import Storage
+from file_storage import FileStorage
+from sqlite_storage import SQLiteStorage
+from sql_alchemy_storage import SQLAlchemyStorage
+
+__all__ = ['Storage', 'FileStorage', 'SQLiteStorage', 'SQLAlchemyStorage']
---
+++ pylofer/storage/sqlite_storage.py
@@ -0,0 +1,4 @@
+from storage import Storage
+
+class SQLiteStorage(Storage):
+    pass
